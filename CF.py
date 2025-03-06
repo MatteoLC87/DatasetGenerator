7ff204a9bd47b2c504fa32204dc8e008d5646dd3
MatteoLC87 <matteo.locicero@gmail.com>
2025-03-06 18:55:18 +0100

    Add files via upload

--- CF.py
+++ CF.py
@@ -89,6 +89,6 @@ def fiscalCodeGenerator(inputName, inputSurname):
     
     controlLetter = alphabet[random.randint(0,25)].upper()
     
-    fiscalCode = ''.join(surnameCF) + ''.join(nameCF) + str(yearCF) + monthCF + str("{:02}".format(dayCF)) + belfioreCodeCF + controlLetter
+    fiscalCode = ''.join(surnameCF) + ''.join(nameCF) + str(yearCF) + monthCF + str(dayCF.zfill(2)) + belfioreCodeCF + controlLetter
     
     return(fiscalCode)
